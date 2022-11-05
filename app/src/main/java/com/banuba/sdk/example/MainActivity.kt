fa0c3bf3890d19f45872613926c046a761f9c5cb
Gleb Prischepa <gleb.prischepa@banuba.com>
2022-11-05 22:09:30 +0300

    Add MainActivity

---
+++ app/src/main/java/com/banuba/sdk/example/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.banuba.sdk.example
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startFaceAR.setOnClickListener {
+            startActivity(Intent(applicationContext, FaceArActivity::class.java))
+        }
+    }
+}
